b28b6f3df7e6ba4118e741e66eeee92a364b8d88
Yang Bai <yangbai@Yolandas-Air.attlocal.net>
2024-04-06 13:06:01 -0500

    init firebase and firestore

---
+++ app/backend/readDoc.js
@@ -0,0 +1,8 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase.config';
+
+export async function readDoc(col, did) {
+    const docRef = doc(db, col, did);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data();
+}
