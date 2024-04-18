93c15f9843e12103d98bf8f5ab38080bb9c44a33
Yang Bai <yangbai@Yolandas-Air.attlocal.net>
2024-04-17 22:40:03 -0500

    init react native stack navigator module + new development build + getDisplayTime.js function

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 // import Authentication from "./frontend/screens/Authentication";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Feed from "./frontend/screens/Feed";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-    return <Feed />;
+    // return <Feed />;
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{
+                headerShown: false
+            }}>
+                <Stack.Screen name='Feed' component={Feed}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
---
+++ backend/helper/getDisplayTime.js
@@ -0,0 +1,47 @@
+export default function getDisplayTime(dateMiliseconds) {
+    const d = new Date(dateMiliseconds);
+    // const d_year = d.getFullYear();
+    const d_month = d.getMonth();
+    const d_weekDay = d.getDay();
+    const d_date = d.getDate();
+    const d_hours = d.getHours();
+    const d_minutes = d.getMinutes();
+
+    const currentDateMiliseconds = Date.now();
+    const diff = currentDateMiliseconds - dateMiliseconds;
+    let returnString;
+
+    if (diff > 604800000) {
+        returnString = `${d_month + 1}/${d_date}`
+    } else if (diff > 86400000 * 2) {
+        switch (d_weekDay) {
+            case 0:
+                returnString = 'Sunday';
+                break;
+            case 1:
+                returnString = 'Monday';
+                break;
+            case 2:
+                returnString = 'Tuesday';
+                break;
+            case 3:
+                returnString = 'Wednesday';
+                break;
+            case 4:
+                returnString = 'Thursday';
+                break;
+            case 5:
+                returnString = 'Friday';
+                break;
+            case 6:
+                returnString = 'Saturday';
+                break;
+        }
+    } else if (diff > 86400000) {
+        returnString = 'Yesterday';
+    } else {
+        returnString = `${d_hours <= 12 ? d_hours : d_hours - 12}:${d_minutes}${d_hours < 12 ? 'am' : 'pm'}`;
+    }
+
+    return returnString;
+}
