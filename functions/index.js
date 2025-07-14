02fc354cf788d82e1a5e4d39c8809ac198d62ac1
Yang Bai <yangbai@Yolandas-Air.attlocal.net>
2025-07-13 20:17:13 -0500

    cloud function to trigger onMessageUpdate

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,17 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { onDocumentUpdated } = require('firebase-functions/v2/firestore');
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+exports.onMessageUpdate = onDocumentUpdated('messages/{cid}', (event) => {
+  const cid = event.params.cid;
+  const before = event.data?.before.data();
+  const after = event.data?.after.data();
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+  const beforeLength = before?.content?.length || 0;
+  const afterLength = after?.content?.length || 0;
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+  if (afterLength > beforeLength) {
+    const newMessages = after.content.slice(beforeLength);
+    console.log(`New messages in ${cid}:`, newMessages);
+  }
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  return null;
+});
